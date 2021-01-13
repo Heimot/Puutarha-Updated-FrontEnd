7ce7917d58408da023556005e2bbdfc20e36f970
Heimot <heimonenjoonas@gmail.com>
2021-01-13 17:30:59 +0200

    Routes and date added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
+import { Switch, Route } from "react-router-dom";
 import Dashboard from './dashboard/dashboard';
-
+import LoginPanel from './loginPanel/loginPanel';
 import './App.css';
 
 function App() {
+  const d = new Date();
+  const ye = new Intl.DateTimeFormat('fi', { year: 'numeric' }).format(d);
+  const mo = new Intl.DateTimeFormat('fi', { month: '2-digit' }).format(d);
+  const da = new Intl.DateTimeFormat('fi', { day: '2-digit' }).format(d);
+
+  sessionStorage.setItem("date", `${da}/${mo}/${ye}`);
+
   return (
     <div className="App">
       <header className="App-header">
-        <Dashboard />
+        <Switch>
+          <Route path="/dashboard"><Dashboard /></Route>
+          <Route path="/" exact><LoginPanel /></Route>
+        </Switch>
       </header>
     </div>
   );
