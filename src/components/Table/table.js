fbbdd26bcd52b6463d17d80243e65dde2f6c0015
Heimot <heimonenjoonas@gmail.com>
2021-01-19 16:48:39 +0200

    Added colors and patching.

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -1,8 +1,53 @@
 import { Table, Thead, Tbody, Th, Tr, Td } from 'react-super-responsive-table';
+import { Input, Button } from 'reactstrap';
+import { normalFetch } from '../Fetch/Fetch';
 import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
 import '../Table/table.css';
 
 function TableUI(props) {
+    const changeValue = (itemState, id) => {
+        let updatedState = "";
+        let maara = document.getElementById(`keratty/${id}`).value;
+        console.log(maara)
+        if (maara === "") {
+            maara = document.getElementById(`keratty/${id}`).placeholder
+        }
+        switch (itemState) {
+            case "Odottaa keräystä":
+                updatedState = "Keräyksessä"
+                break;
+            case "Keräyksessä":
+                updatedState = "Kerätty"
+                break;
+            case "Kerätty":
+                updatedState = "Ei ole"
+                break;
+            case "Ei ole":
+                updatedState = "Odottaa keräystä"
+                break;
+            default:
+                break;
+        }
+        normalFetch(`products/patch/id/` + id, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+            },
+            body: JSON.stringify([
+                {
+                    propName: "keratty",
+                    value: updatedState,
+                },
+                {
+                    propName: "kerattymaara",
+                    value: maara
+                }
+            ])
+        })
+
+        //socket tähän
+    }
     return (
         <Table>
             <Thead>
@@ -23,8 +68,15 @@ function TableUI(props) {
                             <Td>{item.toimi}</Td>
                             <Td>{item.kerays}</Td>
                             <Td>{item.lisatieto}</Td>
-                            <Td>{item.keratty}</Td>
-                            <Td>{item.kerattymaara}</Td>
+                            <Td className="inputTable">
+                                <Input
+                                    className={item.keratty === "Odottaa keräystä" ? "ok" : item.keratty === "Keräyksessä" ? "kss" : item.keratty === "Kerätty" ? "k" : item.keratty === "Ei ole" ? "eo" : "kss"}
+                                    onClick={() => changeValue(item.keratty, item._id)}
+                                    type="button"
+                                    value={item.keratty}>
+                                </Input>
+                            </Td>
+                            <Td className="inputValueTable"><Input type="number" className="tableText" id={`keratty/${item._id}`} placeholder={item.kerattymaara}></Input></Td>
                         </Tr>
                     )
                 })}
