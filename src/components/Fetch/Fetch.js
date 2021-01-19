b3bbffff1975c2ed6d6f7212272d49e6cb245182
Heimot <heimonenjoonas@gmail.com>
2021-01-19 16:48:03 +0200

    Made some changed to fetch functions.

--- src/components/Fetch/Fetch.js
+++ src/components/Fetch/Fetch.js
@@ -1,31 +1,10 @@
-import React from 'react';
+const FETCH_URL = "http://localhost:3002/";
 
-export const useFetch = (url, query, options) => {
-    const [response, setResponse] = React.useState(null);
-    const [error, setError] = React.useState(null);
-    const [isLoading, setIsLoading] = React.useState(false);
-    React.useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-            try {
-                const res = await fetch(url + query, options);
-                const json = await res.json();
-                setResponse(json);
-                setIsLoading(false);
-            } catch (error) {
-                setError(error);
-            }
-        }
-        fetchData();
-    }, [])
-    return { response, error, isLoading };
-};
-
-export const normalFetch = async (url, options) => {
+export const normalFetch = async (query, options) => {
     let res = "";
     let json = {};
     try {
-        res = await fetch(url, options);
+        res = await fetch(FETCH_URL + query, options);
         json = await res.json();
     } catch (error) {
         console.log(error);
