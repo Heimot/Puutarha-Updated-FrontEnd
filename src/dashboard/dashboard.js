af1630675b4a0f34d2c838c43259f5a054edf495
Heimot <heimonenjoonas@gmail.com>
2021-01-21 17:50:16 +0200

    Added socket connection

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -3,16 +3,21 @@ import { useState, useEffect } from 'react';
 import SearchBar from '../components/SearchBar/searchBar';
 import '../dashboard/dashboard.css';
 import { normalFetch } from '../components/Fetch/Fetch';
+import socket from '../components/Sockets/socket-ioConn';
+
+let flo = "";
+let sto = "";
 
 function Dashboard() {
-    let [type, setType] = useState(false);
-    let [flower, setFlower] = useState("");
-    let [store, setStore] = useState("");
-    let [item, setItem] = useState(null);
+    const [type, setType] = useState(false);
+    const [flower, setFlower] = useState("");
+    const [store, setStore] = useState("");
+    const [item, setItem] = useState([]);
+    const [object, setObject] = useState([]);
 
     useEffect(() => {
         async function getData() {
-            let res = await normalFetch(`orders/tables?date=${sessionStorage.getItem("date")}&valmis=Ei&kerays=Ryönä&kukka=${flower}&kauppa=${store}`, {
+            let res = await normalFetch(`orders/tables?date=${sessionStorage.getItem("date")}&valmis=Ei&kerays=Ryönä&kukka=${flo}&kauppa=${sto}`, {
                 method: 'GET',
                 headers: {
                     'Content-Type': 'application/json',
@@ -22,11 +27,36 @@ function Dashboard() {
             setItem(res.product)
         }
         getData();
-
-        return () => {
-            //
-        }
     }, [flower, store])
+
+    useEffect(() => {
+        socket.on('chat', async (data) => {
+            if (data.message === true) {
+                getTable(data.id);
+            };
+        });
+        socket.on('idUpdate', async (data) => {
+            if (data.message === true) {
+                getTable(data.id);
+            };
+        });
+    }, [])
+
+    useEffect(() => {
+        let update = item.map(obj => object.find(o => o._id === obj._id) || obj);
+        setItem(update);
+    }, [object])
+
+    const getTable = async (id) => {
+        let e = await normalFetch(`orders/get/id/${id}?paikka=&valmis=Ei&kukka=${flo}&kauppa=${sto}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem("token")
+            },
+        });
+        setObject([e]);
+    }
 
     if (item === null) {
         return <div>Loading....</div>
@@ -37,6 +67,8 @@ function Dashboard() {
             if (s.length <= 0) {
                 setFlower("");
                 setStore("");
+                flo = "";
+                sto = "";
                 setType(false);
             }
         }
@@ -44,42 +76,24 @@ function Dashboard() {
 
     const searchFilter = async (s) => {
         setType(true);
-        let kukka = "";
         switch (s.searchOption) {
             case "Kauppa":
                 setType(true);
                 setStore(s.search);
+                sto = s.search;
+                flo = "";
                 break;
             case "Kukka":
                 setType(true);
                 setFlower(s.search);
+                flo = s.search;
+                sto = "";
                 break;
             default:
                 //  
                 break;
         }
 
-    }
-
-
-    // REMOVE after testing ended
-    const post = async () => {
-        const response = await fetch("http://localhost:3002/products/post", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluIiwidXNlcklkIjoiNWZlYjQzOTMwYmZlNGMyMzI4NGEzZTdiIiwicm9sZXMiOiJBZG1pbiIsImlhdCI6MTYxMDM2Nzk0MCwiZXhwIjoxNjEwNDU0MzQwfQ.Yy9KbWXYxFjVT9JRB2A7V5ZkpSNReedCylo0NhzmkCg'
-            },
-            body: JSON.stringify({
-                kukka: "Orvokki amppeli punainen",
-                toimi: "100",
-                kerays: "",
-                keratty: "",
-                kerattymaara: "",
-                lisatieto: ""
-            })
-        });
-        console.log(response.json());
     }
 
     return (
