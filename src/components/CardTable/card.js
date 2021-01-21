2b3cf9a49d64ae45a86ffeff30ae43469e5cfb6c
Heimot <heimonenjoonas@gmail.com>
2021-01-21 17:49:01 +0200

    Added id

--- src/components/CardTable/card.js
+++ src/components/CardTable/card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
 import TableUI from '../Table/table';
 import 'bootstrap/dist/css/bootstrap.css';
 
@@ -7,8 +7,9 @@ function CardTable(props) {
     return props.items.map((item) => (
             <Card key={item._id} className="text-dark">
                 <CardTitle>{item.kauppa}</CardTitle>
+                <CardText>{item._id}</CardText>
                 <CardBody className="border rounded">
-                    <TableUI products={item.products} />
+                    <TableUI products={item.products} id={item._id} />
                 </CardBody>
             </Card>
     ))
