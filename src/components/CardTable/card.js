cac21624f352d8b21a7f70d187f8192b18c94978
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-02-06 15:57:08 +0200

    Added more data to card tables.

--- src/components/CardTable/card.js
+++ src/components/CardTable/card.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { Card, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Card, CardBody, CardText, Button } from 'reactstrap';
 import TableUI from '../Table/table';
 import 'bootstrap/dist/css/bootstrap.css';
+import './card.css';
 
 function CardTable(props) {
     return props.items.map((item) => (
-            <Card key={item._id} className="text-dark">
-                <CardTitle>{item.kauppa}</CardTitle>
-                <CardText>{item._id}</CardText>
-                <CardBody className="border rounded">
-                    <TableUI products={item.products} id={item._id} />
-                </CardBody>
-            </Card>
+        <Card key={item._id} className="text-dark">
+            <div className="cardTableOrder">
+                <div className="cardTitles">
+                    <CardText>Keräyspäivämäärä: {item.date}</CardText>
+                    <CardText>{item.kauppa}</CardText>
+                    <CardText>{item._id}</CardText>
+                </div>
+                <div className="cardLisatiedot">
+                    <CardText>{item.alisatieto}</CardText>
+                    <CardText>{item.orderLisatieto}</CardText>
+                </div>
+            </div>
+            <CardBody className="border rounded">
+                <TableUI products={item.products} id={item._id} />
+                <Button>Delete</Button>
+            </CardBody>
+        </Card>
     ))
 }
 
