0877b278f5106ef49b3a156906de2763940520e5
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-02-07 15:47:29 +0200

    Added deleting of orders.

--- src/components/CardTable/card.js
+++ src/components/CardTable/card.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import { Card, CardBody, CardText, Button } from 'reactstrap';
+import { socketConnChat } from '../Sockets/socketio';
+import { normalFetch } from '../Fetch/Fetch';
 import TableUI from '../Table/table';
 import 'bootstrap/dist/css/bootstrap.css';
 import './card.css';
 
+
 function CardTable(props) {
+    const DeleteOrder = async (item) => {
+        item.products.map(async (product) => {
+            await normalFetch('products/delete/id/' + product._id, {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+                }
+            })
+        })
+
+        await normalFetch('orders/delete/id/' + item._id, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + sessionStorage.getItem('token')
+            }
+        })
+        console.log("Order deleted!")
+        socketConnChat();
+    }
+
     return props.items.map((item) => (
         <Card key={item._id} className="text-dark">
             <div className="cardTableOrder">
@@ -20,7 +45,7 @@ function CardTable(props) {
             </div>
             <CardBody className="border rounded">
                 <TableUI products={item.products} id={item._id} />
-                <Button>Delete</Button>
+                <Button onClick={() => DeleteOrder(item)}>Delete</Button>
             </CardBody>
         </Card>
     ))
