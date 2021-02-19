32f9528c22305603ac71295a29ce07c2ffa2087b
Joonas Heimonen <heimonenjoonas@gmail.com>
2021-02-19 15:45:32 +0200

    Added datepicking for nav

--- src/components/NavData/navData.js
+++ src/components/NavData/navData.js
@@ -1,7 +1,27 @@
+import DatePicker from 'react-datepicker';
+import React, { useState, useEffect } from 'react';
 import { Button } from "reactstrap";
 import './navData.css';
 
+import "react-datepicker/dist/react-datepicker.css";
+
 function NavData(props) {
+    const [startDate, setStartDate] = useState(new Date());
+
+    useEffect(() => {
+        let dateS = sessionStorage.getItem('date').split('/');
+        let newDate = `${dateS[1]}/${dateS[0]}/${dateS[2]}`;
+        setStartDate(new Date(newDate));
+    }, [])
+
+    const changeDate = (date) => {
+        setStartDate(date);
+        const ye = new Intl.DateTimeFormat('fi', { year: 'numeric' }).format(date);
+        const mo = new Intl.DateTimeFormat('fi', { month: '2-digit' }).format(date);
+        const da = new Intl.DateTimeFormat('fi', { day: '2-digit' }).format(date);
+        sessionStorage.setItem("date", `${da}/${mo}/${ye}`);
+        window.location.reload()
+    }
 
     return (
         <div className="navData">
@@ -10,7 +30,7 @@ function NavData(props) {
             </div>
             <Button className="navBtn">Kerättävät</Button>
             <Button className="navBtn">Ryönä</Button>
-            <Button className="navBtn">pvm systeemi tähä</Button>
+            <DatePicker className="datePicker" value={sessionStorage.getItem('date')} selected={startDate} onChange={(date) => changeDate(date)} withPortal />
             <Button className="navBtn" onClick={() => props.close()}>Lisää uusi tilaus</Button>
             <Button className="navBtn">Eteneminen</Button>
             <Button className="navBtn">Kirjaudu ulos</Button>
